601e49672081fa5301c40a7ac96a731467910252
Wiley Marques <wilymarques@live.com>
2019-05-21 16:31:50 -0300

    Changing axios to node-fetch

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
-// Import the axios library, to make HTTP requests
-const axios = require('axios');
+const fetch = require('node-fetch');
 
 // This is the client ID and client secret that you obtained
 // while registering the application
@@ -15,23 +13,30 @@ app.get('/oauth/redirect', (req, res) => {
     // The req.query object has the query params that
     // were sent to this route. We want the `code` param
     const requestToken = req.query.code;
-    axios({
-        // make a POST request
-        method: 'post',
-        // to the Github authentication API, with the client ID, client secret
-        // and request token
-        url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-        // Set the content type header, so that we get the response in JSOn
-        headers: {
-            accept: 'application/json',
-        },
-    }).then(response => {
-        // Once we get the response, extract the access token from
-        // the response body
-        const accessToken = response.data.access_token;
-        // redirect the user to the welcome page, along with the access token
-        res.redirect(`/welcome.html?access_token=${accessToken}`);
-    });
+
+    const method = 'POST';
+    const url = `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`;
+    const headers = {
+        accept: 'application/json',
+    };
+
+    fetch(url, {
+        method,
+        headers,
+    })
+        .then(response => response.json())
+        .then(response => {
+            // Once we get the response, extract the access token from
+            // the response body
+            // const responseJson = response.json();
+            console.log('response', response);
+            const accessToken = response.access_token;
+            // redirect the user to the welcome page, along with the access token
+            res.redirect(`/welcome.html?access_token=${accessToken}`);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
 
 app.use(express.static(__dirname + '/public'));
